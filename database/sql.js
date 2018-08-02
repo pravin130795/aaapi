f8b3f6bf79e7344ebdd13d2186ca9adbb5ebd45e
parthraval102 <parth.raval@brainvire.com>
2018-08-02 12:13:36 +0530

    Connection Related Changes

--- database/sql.js
+++ database/sql.js
@@ -3,17 +3,18 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const sequelizeTransforms = require('sequelize-transforms');
 const config = require('../configurations/config');
+const constants = require('../utils/constants');
 
 let sequelize = new Sequelize(config.get('sql.database'), config.get('sql.username'), config.get('sql.password'), config.get('sql'));
 let db = {};
 sequelizeTransforms(sequelize);
 //To Read All Files from the database directory and we will exclude index.js from the directory
-fs.readdirSync(__dirname)
+fs.readdirSync('./' + constants.moduleNames.model + '/')
     .filter(function (file) {
         return (file.indexOf(".") !== 0 && file !== "index.js")
     })
     .forEach(function (file) {
-        let model = sequelize.import(path.join(__dirname, file));
+        let model = sequelize.import('../' + constants.moduleNames.model + '/' + file);
     });
 
 Object.keys(db).forEach(function (modelName) {
